68b8a01d14912d637effeca6ddbb5697a13ddd5f
Arbaj_1147 <shaikharbaj2001@gmail.com>
2024-02-27 21:53:15 +0530

    change

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require("jsonwebtoken")
 require('dotenv').config();
 const app = express();
 
@@ -11,6 +12,28 @@ app.get("/",async(req,res)=>{
 })
 app.get("/hello",async(req,res)=>{
            res.send("Hello");
+})
+
+app.get("/register",async(req,res)=>{
+          const token = await jwt.sign({name:'arbaj'},process.env.JWT_SECRET,{
+               expiresIn:'1h'
+          })
+         return res.json({
+              success:true,
+              token
+          })
+})          
+app.get("/login/:id",async(req,res)=>{
+     try {
+        const token = req.params.id;
+     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+     if(decoded){
+          return res.json({decoded});
+     }
+     } catch (error) {
+          res.send(error);
+     }
+     
 })
 
 app.listen(process.env.PORT,()=>{
